44beadc7587c7e13eed028791c391571a2a24113
Kevin Kimball <kwkimball@gmail.com>
2015-02-28 10:50:30 -0800

    Add values to hash from message

--- lib/sparkpost_rails/delivery_method.rb
+++ lib/sparkpost_rails/delivery_method.rb
@@ -16,20 +16,28 @@ module SparkpostRails
           :click_tracking => SparkpostRails.configuration.track_clicks
         },
         :campaign_id => "",
-        :recipients => {},
+        :recipients => [
+          {
+            :address => {
+              # :name   => "",
+              :email  => mail.to.first
+            }
+          }
+        ],
         :content => {
           :from => {
-            :name   => "",
-            :email  => ""
+            # :name   => "",
+            :email  => mail.from.first
           },
-          :subject  => "",
-          :reply_to => "",
-          :text     => "",
-          :html     => ""
+          :subject  => mail.subject,
+          :reply_to => mail.reply_to.first,
+          :text     => mail.text_part,
+          :html     => mail.html_part
         }
       }
-      puts mail.inspect
-      # headers = { "Authorization" => SparkpostRails.configuration.api_key }
+      puts data
+      headers = { "Authorization" => SparkpostRails.configuration.api_key }
+      puts headers
       # post('/transmissions', { headers: headers, body: data })
       @response = false
     end
