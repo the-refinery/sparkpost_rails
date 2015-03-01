cbc913f7fc6f8ce68b5d6e627cb98a4f06c581a1
Kevin Kimball <kwkimball@gmail.com>
2015-03-01 00:10:59 -0800

    Add return response setting

--- lib/sparkpost_rails/railtie.rb
+++ lib/sparkpost_rails/railtie.rb
@@ -2,7 +2,7 @@ module SparkpostRails
   class Railtie < Rails::Railtie
     initializer "sparkpost_rails.add_delivery_method" do
       ActiveSupport.on_load :action_mailer do
-        ActionMailer::Base.add_delivery_method :sparkpost, SparkpostRails::DeliveryMethod
+        ActionMailer::Base.add_delivery_method :sparkpost, SparkpostRails::DeliveryMethod, return_response: true
       end
     end
   end
