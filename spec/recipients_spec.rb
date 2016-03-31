bb5e868e96e1f3b660d33a91026f2b34fca99167
Dave Goerlich <dgoerlich@designinginteractive.com>
2016-03-31 15:43:54 -0400

    The basic testing structure in place, and the first spec - recipients all green

---
+++ spec/recipients_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe SparkPostRails::DeliveryMethod do
+
+  before(:each) do
+    @delivery_method = SparkPostRails::DeliveryMethod.new
+  end
+
+  context "Recipients" do
+
+    context "single recipient" do
+      it "handles email only" do
+        test_email = Mailer.test_email
+        @delivery_method.deliver!(test_email)
+
+        expect(@delivery_method.data[:recipients]).to eq([{address: {email: "to@example.com"}}])
+      end
+
+      it "handles name and email" do
+        test_email = Mailer.test_email to: "Joe Test <to@example.com>"
+        @delivery_method.deliver!(test_email)
+
+        expect(@delivery_method.data[:recipients]).to eq([{address: {email: "to@example.com", name: "Joe Test"}}])
+      end
+    end
+
+    context "multiple recipients" do
+      it "handles email only" do
+        test_email = Mailer.test_email to: "to1@example.com, to2@example.com"
+        @delivery_method.deliver!(test_email)
+
+        expect(@delivery_method.data[:recipients]).to eq([{address: {email: "to1@example.com"}}, {address: {email: "to2@example.com"}}])
+      end
+
+      it "handles name and email" do
+        test_email = Mailer.test_email to: "Sam Test <to1@example.com>, Joe Test <to2@example.com>"
+        @delivery_method.deliver!(test_email)
+
+        expect(@delivery_method.data[:recipients]).to eq([{:address=>{:email=>"to1@example.com", :name=>"Sam Test"}}, {:address=>{:email=>"to2@example.com", :name=>"Joe Test"}}])
+      end
+
+      it "handles mix of email only and name/email" do
+        test_email = Mailer.test_email to: "Sam Test <to1@example.com>, to2@example.com"
+        @delivery_method.deliver!(test_email)
+
+        expect(@delivery_method.data[:recipients]).to eq([{:address=>{:email=>"to1@example.com", :name=>"Sam Test"}}, {:address=>{:email=>"to2@example.com"}}])
+      end
+
+    end
+
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,35 @@
+require 'webmock/rspec'
+require 'rails'
+require 'action_mailer'
+require "sparkpost_rails"
+
+RSpec.configure do |config|
+
+  config.before(:all) do
+    SparkPostRails.configure do |c|
+      c.api_key = "TESTKEY1234"
+    end
+  end
+
+  config.before(:each) do
+    stub_request(:any, "https://api.sparkpost.com/api/v1/transmissions").
+      to_return(body: "{\"response\":{\"total_rejected_recipients\":0,\"total_accepted_recipients\":1,\"id\":\"00000000000000000\"}}", status: 200)
+  end
+
+end
+
+#A default mailer to generate the mail object
+class Mailer < ActionMailer::Base
+  def test_email options = {}
+    data = {
+      from: "from@example.com",
+      to: "to@example.com",
+      subject: "Test Email",
+      body: "Hello, Testing!"
+    }
+
+    data.merge! options
+
+    mail(data)
+  end
+end
