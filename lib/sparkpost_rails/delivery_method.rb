414df68fa88968bc66397423c98c9a15255ecc46
Norbert Szivos <sysqa@yahoo.com>
2017-11-29 16:02:18 +0100

    Use cid instead of url for the inline attachments

--- lib/sparkpost_rails/delivery_method.rb
+++ lib/sparkpost_rails/delivery_method.rb
@@ -188,7 +188,7 @@ module SparkPostRails
       mail.attachments.each do |attachment|
         #We decode and reencode here to ensure that attachments are
         #Base64 encoded without line breaks as required by the API.
-        attachment_data = { name: attachment.inline? ? attachment.url : attachment.filename,
+        attachment_data = { name: attachment.inline? ? attachment.cid : attachment.filename,
                             type: attachment.content_type,
                             data: Base64.strict_encode64(attachment.body.decoded) }
 
